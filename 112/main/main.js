058df215c443dd26ecf2d7a8ec01c44792632b78
严泽凡 <18231550929@sina.com>
2017-12-01 20:59:53 +0800

    112重构

--- 112/main/main.js
+++ 112/main/main.js
@@ -3,51 +3,38 @@ const loadPromotions = require("./datbase").loadPromotions;
 
 "use strict";
 
-module.exports = function printInventory(inputs){
-    var cart = format(inputs);
-    var promotion = getPromotion(cart);
-    var result = getTotal(cart, promotion);
+module.exports.printInventory = function printInventory(inputs){
+    var cart = this.format(inputs);
+    var promotion = this.getPromotion(cart);
+    var result = this.getTotal(cart, promotion);
     return result;
+}
 
-    /**
-        format 一个订单数组形如:
-        {
-            barcode: 'ITEM000000',
-            name: '可口可乐',
-            unit: '瓶',
-            price: 3.00,
-            count: 3,
-            subtotal: 9     //总价
+/**
+    format 一个订单数组形如:
+    {
+        barcode: 'ITEM000000',
+        name: '可口可乐',
+        unit: '瓶',
+        price: 3.00,
+        count: 3,
+        subtotal: 9     //总价
+    }
+**/
+module.exports.format = function format(inputs){
+    var result = [];
+    var allItems = loadAllItems();
+    for (var i = 0; i < inputs.length;) {
+        var count = 0;
+        var map ={};
+        for (var j = i; j < inputs.length; j++) {
+            if(inputs[i] == inputs[j]){
+                count++;
+            }
         }
-    **/
-    function format(inputs){
-        var result = [];
-        var allItems = loadAllItems();
-        for (var i = 0; i < inputs.length;) {
-            var count = 0;
-            var map ={};
-            for (var j = i; j < inputs.length; j++) {
-                if(inputs[i] == inputs[j]){
-                    count++;
-                }
-            }
-            if(inputs[i].indexOf('-') > 0){
-                map['barcode'] = inputs[i].split("-")[0];
-                map['count'] = parseInt(inputs[i].split("-")[1]);
-                for (var k = 0; k < allItems.length; k++) {
-                    if(map.barcode == allItems[k].barcode){
-                        map.name = allItems[k].name;
-                        map.unit = allItems[k].unit;
-                        map.price = allItems[k].price;
-                        map.subtotal = map.count * map.price; 
-                    }
-                }
-                result.push(map);
-                i++;
-                continue;
-            }
-            map.barcode = inputs[i];
-            map.count = count;
+        if(inputs[i].indexOf('-') > 0){
+            map['barcode'] = inputs[i].split("-")[0];
+            map['count'] = parseInt(inputs[i].split("-")[1]);
             for (var k = 0; k < allItems.length; k++) {
                 if(map.barcode == allItems[k].barcode){
                     map.name = allItems[k].name;
@@ -57,72 +44,84 @@ module.exports = function printInventory(inputs){
                 }
             }
             result.push(map);
-            i += count;
+            i++;
+            continue;
         }
-        return result;
-    }
-
-    /**
-        获得优惠商品
-    **/
-    function getPromotion(cart){
-        var promotion = [];
-        var nowPromotions = loadPromotions()[0]["barcodes"];    //真坑。
-        for (var element in cart) {
-            for (var item in nowPromotions) {
-                if(cart[element]["barcode"] == nowPromotions[item] && cart[element]["count"] > 2){
-                    promotion.push({
-                        "barcode": cart[element]["barcode"],
-                        "name": cart[element]["name"],
-                        "count": 1,  
-                        "price": cart[element]["price"],
-                        "unit": cart[element]["unit"]
-                    });
-                }
+        map.barcode = inputs[i];
+        map.count = count;
+        for (var k = 0; k < allItems.length; k++) {
+            if(map.barcode == allItems[k].barcode){
+                map.name = allItems[k].name;
+                map.unit = allItems[k].unit;
+                map.price = allItems[k].price;
+                map.subtotal = map.count * map.price; 
             }
         }
-        return promotion;
+        result.push(map);
+        i += count;
     }
-
-    /**
-        合计+打印
-    **/
-    function getTotal(cart, promotion){
-        var total = 0;
-        var promotions = 0;
-        for(var i in cart){
-            total += cart[i]["subtotal"];
-        }
-        for(var i in promotion){
-            promotions += promotion[i]["price"] * promotion[i]["count"];
-        }
-        total -= promotions;
-        var result = {"total": total, "promotions": promotions};
-
-
-        var expectText = '***<没钱赚商店>购物清单***\n'
-        for(var i in cart){
-            expectText += '名称：' + cart[i]["name"] + "，数量：" + cart[i]["count"] + cart[i]["unit"] + "，单价：" + cart[i]["price"].toFixed(2) + "(元)，小计：";
-            var subtotal = cart[i]["subtotal"];
-            for(var j in promotion){
-                if(cart[i]["barcode"] == promotion[j]["barcode"]){
-                    subtotal -= parseFloat(promotion[j]["price"]);
-                }
-            }
-            subtotal = parseFloat(subtotal).toFixed(2);
-            expectText += subtotal + "(元)\n";
-        }
-        expectText += "----------------------\n";
-        expectText += '挥泪赠送商品：\n';
-        for(var k in promotion){
-            expectText += "名称：" + promotion[k]["name"] + "，数量：" + promotion[k]["count"] + promotion[k]["unit"] + "\n";
-        }
-        expectText += "----------------------\n";
-        expectText += "总计：" + result["total"].toFixed(2) + "(元)\n";
-        expectText += "节省：" + result["promotions"].toFixed(2) + "(元)\n";
-        expectText += "**********************";
-
-        return expectText;
-    }
+    return result;
 }
 
+/**
+    获得优惠商品
+**/
+module.exports.getPromotion = function getPromotion(cart){
+    var promotion = [];
+    var nowPromotions = loadPromotions()[0]["barcodes"];    //真坑。
+    for (var element in cart) {
+        for (var item in nowPromotions) {
+            if(cart[element]["barcode"] == nowPromotions[item] && cart[element]["count"] > 2){
+                promotion.push({
+                    "barcode": cart[element]["barcode"],
+                    "name": cart[element]["name"],
+                    "count": 1,  
+                    "price": cart[element]["price"],
+                    "unit": cart[element]["unit"]
+                });
+            }
+        }
+    }
+    return promotion;
+}
+
+/**
+    合计+打印
+**/
+module.exports.getTotal = function getTotal(cart, promotion){
+    var total = 0;
+    var promotions = 0;
+    for(var i in cart){
+        total += cart[i]["subtotal"];
+    }
+    for(var i in promotion){
+        promotions += promotion[i]["price"] * promotion[i]["count"];
+    }
+    total -= promotions;
+    var result = {"total": total, "promotions": promotions};
+
+
+    var expectText = '***<没钱赚商店>购物清单***\n'
+    for(var i in cart){
+        expectText += '名称：' + cart[i]["name"] + "，数量：" + cart[i]["count"] + cart[i]["unit"] + "，单价：" + cart[i]["price"].toFixed(2) + "(元)，小计：";
+        var subtotal = cart[i]["subtotal"];
+        for(var j in promotion){
+            if(cart[i]["barcode"] == promotion[j]["barcode"]){
+                subtotal -= parseFloat(promotion[j]["price"]);
+            }
+        }
+        subtotal = parseFloat(subtotal).toFixed(2);
+        expectText += subtotal + "(元)\n";
+    }
+    expectText += "----------------------\n";
+    expectText += '挥泪赠送商品：\n';
+    for(var k in promotion){
+        expectText += "名称：" + promotion[k]["name"] + "，数量：" + promotion[k]["count"] + promotion[k]["unit"] + "\n";
+    }
+    expectText += "----------------------\n";
+    expectText += "总计：" + result["total"].toFixed(2) + "(元)\n";
+    expectText += "节省：" + result["promotions"].toFixed(2) + "(元)\n";
+    expectText += "**********************";
+
+    return expectText;
+}
--- 112/spec/main-spec.js
+++ 112/spec/main-spec.js
@@ -1,5 +1,7 @@
-var printInventory = require('../main/main');
+const main = require('../main/main');
 const datbase  = require('../main/datbase');
+
+
 
 
 describe('pos', function () {
@@ -21,11 +23,86 @@ describe('pos', function () {
         ];
     });
 
+    it("is format correct ?", function(){
+        var result = main.format(inputs);
+        var expectText =[
+                { 
+                    barcode: 'ITEM000001', 
+                    count: 5, name: '雪碧', 
+                    unit: '瓶', 
+                    price: 3, 
+                    subtotal: 15 
+                }, 
+                { 
+                    barcode: 'ITEM000003', 
+                    count: 2, 
+                    name: '荔枝', 
+                    unit: '斤',
+                    price: 15, 
+                    subtotal: 30 
+                 }, 
+                 { 
+                    barcode: 'ITEM000005', 
+                    count: 3, 
+                    name: '方便面', 
+                    unit: '袋',
+                    price: 4.5, 
+                    subtotal: 13.5 
+                }
+        ];
+
+        expect(result).toEqual(expectText);
+    });
+
+    it("is getPromotion correct ?", function(){
+        var cart = main.format(inputs);
+        var result = main.getPromotion(cart);
+        var expectText =[ 
+            { 
+                barcode: 'ITEM000001', 
+                name: '雪碧', 
+                count: 1, 
+                price: 3, 
+                unit: '瓶'
+            },
+            { 
+                barcode: 'ITEM000005', 
+                name: '方便面', 
+                count: 1, 
+                price: 4.5, 
+                unit: '袋' 
+            }
+        ];
+
+        expect(result).toEqual(expectText);
+    });
+
+     it("is getTotal correct ?", function(){
+        var cart = main.format(inputs);
+        var promotion = main.getPromotion(cart);
+        var result = main.getTotal(cart,promotion);
+        var expectText =
+         '***<没钱赚商店>购物清单***\n' +
+            '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
+            '名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n' +
+            '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n' +
+            '----------------------\n' +
+            '挥泪赠送商品：\n' +
+            '名称：雪碧，数量：1瓶\n' +
+            '名称：方便面，数量：1袋\n' +
+            '----------------------\n' +
+            '总计：51.00(元)\n' +
+            '节省：7.50(元)\n' +
+            '**********************';
+
+        expect(result).toEqual(expectText);
+    });
+
     it('should print correct text', function () {
 
         spyOn(console, 'log');
 
-        var result =printInventory(inputs);
+        var result = main.printInventory(inputs);
 
 
         var expectText =
@@ -44,6 +121,5 @@ describe('pos', function () {
 
         expect(result).toEqual(expectText);
        
-        // expect(result).toHaveBeenCalledWith(expectText);
     });
 });
