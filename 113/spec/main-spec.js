60262ed2f95175dc5a552116c674a01f9c44f788
严泽凡 <18231550929@sina.com>
2017-12-01 21:05:41 +0800

    113 重构

--- 113/spec/main-spec.js
+++ 113/spec/main-spec.js
@@ -1,7 +1,23 @@
 const printInventory = require('../main/main');
 
 describe('taxi fee', function () {
-    it("taxi",function(){
+    
+
+    it("0 < x < 2", () => {
+    	var kilometers = 2;
+    	var time = 10;
+    	var result = printInventory(kilometers,time);
+    	expect(result).toEqual(9);
+    });
+
+    it("2 < x < 8", () => {
+    	var kilometers = 6;
+    	var time = 10;
+    	var result = printInventory(kilometers,time);
+    	expect(result).toEqual(12);
+    });
+
+    it("x > 8",function(){
     	var kilometers = 10;
     	var time = 10;
     	var result = printInventory(kilometers,time);
@@ -10,4 +26,5 @@ describe('taxi fee', function () {
 
     	expect(result).toEqual(expectText);
     });
+
 });
