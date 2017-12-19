bb5b0db6a410cc75554b609ad24c71b4a1a403e8
严泽凡 <18231550929@sina.com>
2017-12-19 22:01:18 +0800

    118

--- 118/src/js/main.js
+++ 118/src/js/main.js
@@ -1,4 +1,38 @@
+/**
+	添加表单元素
+*/
 $("#submitSelect").click(function(){
-	alert($("#selectValue").val());
-	alert("1");
+	var selectValue = $("#selectValue").val();
+	if(selectValue != ""){
+		$("#wordList").append(`
+ 			<tr>
+		         <td>`+ selectValue +`</td>
+		         <td>
+		         	<button class="deleteTheWord">删除</button>
+			    	
+		         </td>
+	      	</tr>
+		`);
+		$(".deleteTheWord").click(function(){
+			$(this).parent().parent("tr").remove();
+		});
+	} else{
+		alert("添加不能为空！！！！！");
+	}
+	$("#cancleSelect").click();
+});
+
+/**
+	切换显示
+*/
+$("#seeAll").click(function(){
+	var seeAll = $("#seeAll").text();
+	if (seeAll == "预览") {
+		$("#seeAll").text("编辑");
+	
+	} else{
+		$("#seeAll").text("预览");
+	}
+	$("#addText1").toggle();
+	$(".deleteTheWord").toggle();
 });
